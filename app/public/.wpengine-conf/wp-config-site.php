10f4943509376aa18ae80b07f89e82d527cb1677
btc-devteam <120042852+btc-devteam@users.noreply.github.com>
2022-12-19 16:02:18 -0600

    Initial commit

---
+++ app/public/.wpengine-conf/wp-config-site.php
@@ -0,0 +1,5 @@
+<?php
+# GENERATED AUTOMATICALLY ON 2022-12-19 15:47:01.151539 -0600 CST m=+124.432761793
+$table_prefix = "wp_";
+$wpengine_primary_original_domain = "btcu2023.wpengine.com";
+$wpengine_primary_original_ip_address = "104.196.180.130";
